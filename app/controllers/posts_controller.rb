dc621fe139c1664c28d95f180acd163676fbf8d8
Jeronimo Maranion <jeronimomaranion@MacBook-Pro-2.lan1>
2021-04-28 12:48:58 -0400

    Re-do seed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :update, :destroy]
+  before_action :authorize_request, only: [:create, :update, :destroy]
+  before_action :set_post, only: :show
+  before_action :set_user_post, only: [:update, :destroy]
 
   # GET /posts
   def index
@@ -16,7 +18,7 @@ class PostsController < ApplicationController
   # POST /posts
   def create
     @post = Post.new(post_params)
-
+    @post.user = @current_user
     if @post.save
       render json: @post, status: :created
     else
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,27 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.destroy_all
-Post.destroy_all
 Comment.destroy_all
+Post.destroy_all
+User.destroy_all
 
-@user = User.create!(username: 'jerry', email: 'jerry@jerry.com', password: '123')
+@jerry = User.create!(username: 'jerry', email: 'jerry@jerry.com', password: '123456')
+@david = User.create!(username: 'david', email: 'david@david.com', password: '123456')
+@stefon = User.create!(username: 'stefon', email: 'stefon@stefon.com', password: '123456')
+@ashley = User.create!(username: 'ashley', email: 'ashley@ashley.com', password: '123456')
+@anna = User.create!(username: 'anna', email: 'anna@anna.com', password: '123456')
 
 puts "#{User.count} users created"
 
-@post = Post.create!(title: 'Banned 1', content: 'Jordan 1 banned', photo_url: 'https://image.goat.com/crop/500/attachments/product_template_additional_pictures/images/045/223/287/original/693245_01.jpg.jpeg?1605033875', price: '300', contact_info: 'jordan@jordan.com')
-
-
+@jordan = Post.create!(user: @jerry, title: 'Banned 1', content: 'Jordan 1 banned', photo_url: 'https://image.goat.com/crop/500/attachments/product_template_additional_pictures/images/045/223/287/original/693245_01.jpg.jpeg?1605033875', price: '300', contact_info: 'jordan@jordan.com'
+)
+@yeezy = Post.create!(user: @david, title: 'Yeezy', content: 'Yeezy 350 V2 Bred', photo_url: 'https://image.goat.com/crop/750/attachments/product_template_additional_pictures/images/008/492/440/original/94407_01.jpg.jpeg?1513117674', price: '500', contact_info: 'yeezy@yeezy.com'
+)
+@airmax = Post.create!(user: @stefon, title: 'Air Max', content: 'Air Max 90', photo_url: 'https://image.goat.com/crop/750/attachments/product_template_additional_pictures/images/050/450/525/original/723428_01.jpg.jpeg?1613585015', price: '250', contact_info: 'airmax@airmax.com'
+)
 puts "#{Post.count} posts created"
 
-@comment = Comment.create!(content: 'These are fire!')
+Comment.create!(user: @jerry, post: @jordan, content: 'These are fire!')
 
 puts "#{Comment.count} comments created"
 
