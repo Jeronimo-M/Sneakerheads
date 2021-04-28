a7cad065e6358d2910f91de48e627208c1c5b173
Jeronimo Maranion <jeronimomaranion@MacBook-Pro-2.lan1>
2021-04-28 19:20:08 -0400

    First render on front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+// packages
+
+// components
+import Layout from './layouts/Layout';
+
 
 function App() {
+ 
+  
   return (
     <div className="App">
-      
+      <Layout>
+        
+      </Layout>
     </div>
   );
 }
+      
 
 export default App;
---
+++ client/src/layouts/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Layout(props) {
+  
+  return (
+    <div>
+      <header>
+        <h1>Sneakerheads</h1>
+        
+            <Link to='/login'>Login/Register</Link>
+        
+      </header>
+      {props.children}
+    </div>
+  )
+}
